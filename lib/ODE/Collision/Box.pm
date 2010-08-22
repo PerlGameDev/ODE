1481c9e861f5700e1f0e87527b5486d54fdf2a2f
Breno G. de Oliveira <garu@cpan.org>
2010-08-22 04:27:50 -0300

    Box documentation

--- lib/ODE/Collision/Box.pm
+++ lib/ODE/Collision/Box.pm
@@ -5,3 +5,61 @@ use warnings;
 bootstrap ODE::Collision::Box;
 
 1;
+__END__
+
+=head1 NAME
+
+ODE::Collision::Box - Box Geometry for Collision Detection
+
+=head1 SYNOPSIS
+
+  use ODE;
+  use ODE::Collision::Box;
+
+  my $box = ODE::Collision::Box->create_box( 3, 2.1, 4 );
+
+=head1 METHODS
+
+=head2 create_box( $lx, $ly, $lz )
+
+B<Parameters:>
+
+$lx - the length of the box along the X axis
+$ly  the length of the box along the Y axis
+$lz  the length of the box along the Z axis
+
+Returns a new box geometry. The point of reference for coordinates is its center.
+
+
+=head2 get_lengths()
+
+  my ($lx, $ly, $lz) = get_lengths();
+
+Get the side lengths of a box.
+
+
+=head2 set_lengths($lx, $ly, $lz)
+
+B<Parameters:>
+
+$lx - the length of the box along the X axis
+$ly  the length of the box along the Y axis
+$lz  the length of the box along the Z axis
+
+Set the side lengths of the given box. Returns nothing.
+
+
+=head1 ACKNOWLEDGEMENTS
+
+Documentation almost taken verbatim from L<ODE's|http://opende.sourceforge.net/docs/group__collide__box.html>.
+
+
+=head1 AUTHORS
+
+See L<ODE/AUTHORS>.
+
+=head1 COPYRIGHT AND LICENSE
+
+See L<ODE/COPYRIGHT AND LICENSE>.
+
+
