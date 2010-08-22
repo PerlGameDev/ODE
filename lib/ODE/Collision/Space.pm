e4db3ee2b2fe2b680828efdc5fceba408ae15701
Kartik Thakore <thakore.kartik@gmail.com>
2010-08-22 14:52:38 -0400

    Added Space missing files

---
+++ lib/ODE/Collision/Space.pm
@@ -0,0 +1,46 @@
+package ODE::Collision::Space;
+use strict;
+use warnings;
+use vars qw(@ISA);
+require DynaLoader;
+our @ISA = qw(DynaLoader);
+
+bootstrap ODE::Collision::Space;
+
+1;
+__END__
+
+=head1 NAME
+
+ODE::Collision::Space - Defines spaces in ODE
+
+=head1 SYNOPSIS
+
+  use ODE;
+  use ODE::Collision::Space;
+
+  my $space = ODE::Collision::Space::simple_space_create();
+
+=head1 METHODS
+
+=head2 simple_space_create( $parent )
+
+B<Parameters:>
+
+$parent - the parent space 
+
+Creates a spaceID to use with collisons. 
+
+=head1 ACKNOWLEDGEMENTS
+
+Documentation from L<ODE's|http://opende.sourceforge.net/wiki/index.php/Manual_%28Collision_Detection%29#Space_functions>.
+
+=head1 AUTHORS
+
+See L<ODE/AUTHORS>.
+
+=head1 COPYRIGHT AND LICENSE
+
+See L<ODE/COPYRIGHT AND LICENSE>.
+
+
---
+++ lib/ODE/Collision/Space.xs
@@ -0,0 +1,28 @@
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+#include "ppport.h"
+
+#include <ode/ode.h>
+
+MODULE = ODE::Collision::Space 		PACKAGE = ODE::Collision::Space		PREFIX = ocsp_
+
+dSpaceID
+ocsp_simple_space_create( ... )
+	CODE:
+	dSpaceID parent = 0;
+	if(items > 1) 	
+	   parent = (dSpaceID)SvIV(ST(1));
+	RETVAL = dSimpleSpaceCreate(parent);
+	OUTPUT:
+	RETVAL
+
+void
+ocsp_DESTROY( space )
+	dSpaceID space
+	CODE:
+		 dSpaceDestroy( space );
+
+
+
+
