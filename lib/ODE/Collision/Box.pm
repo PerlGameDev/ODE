d1c9d2b54c5688dde2e301812e9bcaa89d22b249
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-05-09 22:06:46 -0400

    Fix some box documentation

--- lib/ODE/Collision/Box.pm
+++ lib/ODE/Collision/Box.pm
@@ -8,6 +8,7 @@ our @ISA = qw(DynaLoader);
 bootstrap ODE::Collision::Box;
 
 1;
+
 __END__
 
 =head1 NAME
@@ -19,43 +20,49 @@ ODE::Collision::Box - Box Geometry for Collision Detection
   use ODE;
   use ODE::Collision::Box;
 
-  my $box = ODE::Collision::Box->create_box( 3, 2.1, 4 );
+  my $box = ODE::Collision::Box::create_box( $space, 3, 2.1, 4 );
 
 =head1 METHODS
 
-=head2 create_box( $lx, $ly, $lz )
+=head2 create_box( $space, $lx, $ly, $lz )
 
 B<Parameters:>
 
+$space - a space to contain the new geom. May be null.
+
 $lx - the length of the box along the X axis
-$ly  the length of the box along the Y axis
-$lz  the length of the box along the Z axis
 
-Returns a new box geometry. The point of reference for coordinates is its center.
+$ly - the length of the box along the Y axis
 
+$lz - the length of the box along the Z axis
 
-=head2 get_lengths()
+Returns a new box geometry. The point of reference for coordinates is
+its center.
 
-  my ($lx, $ly, $lz) = get_lengths();
+=head2 get_lengths($box)
+
+  my ($lx, $ly, $lz) = ODE::Collision::Box::get_lengths($box);
 
 Get the side lengths of a box.
 
+=head2 set_lengths($box, $lx, $ly, $lz)
 
-=head2 set_lengths($lx, $ly, $lz)
+  ODE::Collision::Box::set_lengths($box, $lx, $ly, $lz);
 
 B<Parameters:>
 
 $lx - the length of the box along the X axis
-$ly  the length of the box along the Y axis
-$lz  the length of the box along the Z axis
+
+$ly - the length of the box along the Y axis
+
+$lz - the length of the box along the Z axis
 
 Set the side lengths of the given box. Returns nothing.
 
-
 =head1 ACKNOWLEDGEMENTS
 
-Documentation almost taken verbatim from L<ODE's|http://opende.sourceforge.net/docs/group__collide__box.html>.
-
+Documentation almost taken verbatim from
+L<ODE's|http://opende.sourceforge.net/docs/group__collide__box.html>.
 
 =head1 AUTHORS
 
@@ -64,5 +71,4 @@ See L<ODE/AUTHORS>.
 =head1 COPYRIGHT AND LICENSE
 
 See L<ODE/COPYRIGHT AND LICENSE>.
-
 
